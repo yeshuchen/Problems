ea4feceda45b93e93c0d53a5152b19a3d72c340c
yeshuchen <hswfwkj@163.com>
2022-08-27 23:16:45 +0800

    构造

---
+++ P8471 [Aya Round 1 F] 琪露诺的选择题（未完成）.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int T, n, a, e;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    cin >> T;
+    while(T--)
+    {
+        string s;
+        cin >> n >> a >> e;
+        cin >> s;
+    }
+    return 0;
+}
