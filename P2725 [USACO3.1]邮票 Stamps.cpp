59819c4699196be2efa91789628dcf8e892e06e3
yeshuchen <hswfwkj@163.com>
2022-09-15 11:54:25 +0800

    dp，背包

--- P2725 [USACO3.1]邮票 Stamps.cpp
+++ P2725 [USACO3.1]邮票 Stamps.cpp
@@ -1,15 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int maxn = 500001;
+const int maxn = 4000001;
 int n, k;
-int a[1000006];
-int f[1000006];
+int a[4000006];
+int f[4000006];
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);cout.tie(0);
     cin >> k >> n;
-    for(int i = 1; i <= maxn; i++) f[i] = 114514;
+    for(int i = 1; i <= maxn; i++) f[i] = 1145141919;
     for(int i = 1; i <= n; i++)
         cin >> a[i];
     for(int i = 1; i <= maxn; i++)
