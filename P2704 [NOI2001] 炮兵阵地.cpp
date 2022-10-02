7342fe343b443fb15556ec754e408e510e527d31
yeshuchen <hswfwkj@163.com>
2022-10-02 12:00:28 +0800

    状压dp好题，预处理或滚动数组

--- P2704 [NOI2001] 炮兵阵地.cpp
+++ P2704 [NOI2001] 炮兵阵地.cpp
@@ -34,13 +34,19 @@ signed main()
         if(!ck(i)) temp[++tot] = i;
     for(int i = 1; i <= n; i++)
         for(int j = 1; j <= tot; j++)
+        {
+            if(chec(temp[j], i - 2)) continue;
             for(int k = 1; k <= tot; k++)
+            {
+                if(chec(temp[k], i - 1)) continue;
                 for(int l = 1; l <= tot; l++)
                 {
-                    if(chec(temp[j], i - 2) || chec(temp[k], i - 1) || chec(temp[l], i)) continue;
+                    if(chec(temp[l], i)) continue;
                     if(!check(temp[j], temp[k]) || !check(temp[k], temp[l]) || !check(temp[j], temp[l])) continue;
                     f[i][l][k] = max(f[i][l][k], f[i - 1][k][j] + c(temp[l]));
                 }
+            }
+        }
     for(int i = 1; i <= tot; i++)
         for(int j = 1; j <= tot; j++)
             ans = max(ans, f[n][j][i]);
