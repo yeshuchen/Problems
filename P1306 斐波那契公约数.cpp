c21743b9c48226f187bc620e5e79ecadd7a324da
yeshuchen <hswfwkj@163.com>
2022-10-08 10:25:02 +0800

    数论，矩阵加速

---
+++ P1306 斐波那契公约数.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+#define int long long
+using namespace std;
+const int mod = 1e8;
+int n, m;
+int f[3][3];
+int a[3][3], b[3][3], c[3][3];
+inline void mule()
+{
+	memset(c, 0, sizeof(c));
+	for(int i = 1; i <= 2; i++)
+		for(int j = 1; j <= 2; j++)
+			for(int k = 1; k <= 2; k++)
+				c[i][j] += b[i][k] * a[k][j], c[i][j] %= mod;
+	memcpy(b, c, sizeof(b));
+}
+inline void mul()
+{
+	memset(c, 0, sizeof(c));
+	for(int i = 1; i <= 2; i++)
+		for(int j = 1; j <= 2; j++)
+			for(int k = 1; k <= 2; k++)
+				c[i][j] += a[i][k] * a[k][j], c[i][j] %= mod;
+	memcpy(a, c, sizeof(a));
+}
+inline void muler()
+{
+	memset(c, 0, sizeof(c));
+	for(int i = 1; i <= 1; i++)
+		for(int j = 1; j <= 2; j++)
+			for(int k = 1; k <= 2; k++)
+				c[i][j] += f[i][k] * b[k][j];
+	cout << c[1][1] % mod << '\n';
+}
+inline void power(int b)
+{
+	while(b)
+	{
+		if(b & 1) mule();
+		b >>= 1;
+		mul();
+	}
+}
+signed main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	cin >> n >> m;
+	n = __gcd(n, m);
+	f[1][1] = f[1][2] = 1;
+	b[1][1] = b[2][2] = 1;
+	a[1][2] = a[2][1] = a[2][2] = 1;
+	power(n - 1);
+	muler();
+	return 0;
+}
