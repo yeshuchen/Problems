dce4d9a97cd5793367bee10796f9b7b5fc3526fc
hswfwkj <hswfwkj@163.com>
2022-10-13 08:29:19 +0800

    区间dp，__int128草过去

--- P1005 [NOIP2007 提高组] 矩阵取数游戏.cpp
+++ P1005 [NOIP2007 提高组] 矩阵取数游戏.cpp
@@ -1,34 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, m;
-int a[105][105];
-__int128 ans, base[105], f[105][105];
-void write(__int128 x)
+int a[85];
+__int128 power[85];
+__int128 ans, f[85][85];
+inline void write(__int128 x)
 {
-    if(x > 9) write(x / 10);
-    putchar(x % 10 + 48);
+	if(x > 9) write(x / 10);
+	putchar(x % 10 + 48);
 }
 int main()
 {
-    cin >> n >> m;
-    base[0] = 1;
-    for(int i = 1; i <= m; i++)
-        base[i] = base[i - 1] * 2;
-    for(int i = 1; i <= n; i++)
-        for(int j = 1; j <= m; j++)
-            cin >> a[i][j];
-    for(int t = 1; t <= n; t++)
-    {
-        for(int len = 1; len <= m; len++)
-            for(int i = 1; i <= m; i++)
-            {
-                int j = i + len - 1;
-                if(j > m) break;
-                f[i][j] = 0;
-                f[i][j] = max(f[i][j], f[i][j - 1] + a[t][j] * base[m - len + 1]);
-                f[i][j] = max(f[i][j], f[i + 1][j] + a[t][i] * base[m - len + 1]);
-            }
-        ans += f[1][m];
-    }
-    write(ans);
+	int n, m;
+	cin >> n >> m;
+	power[0] = 1;
+	for(int i = 1; i <= m; i++) power[i] = power[i - 1] * 2;
+	for(int i = 1; i <= n; i++)
+	{
+		for(int j = 1; j <= m; j++)
+			cin >> a[j];
+		for(int j = 1; j <= m; j++) f[j][j] = (__int128)a[j] * power[m];
+		for(int len = 2; len <= m; len++)
+		{
+			for(int j = 1; j <= m; j++)
+			{
+				int k = j + len - 1;
+				if(k > m) break;
+				f[j][k] = max(f[j][k - 1] + a[k] * power[m - len + 1], f[j + 1][k] + a[j] * power[m - len + 1]);
+			}
+		}
+		ans += f[1][m];
+	}
+	write(ans);
+	return 0;
 }
