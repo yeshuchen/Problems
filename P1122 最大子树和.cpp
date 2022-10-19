a5f85cd6cc953d17cd08c32de8f21f5459ca4605
hswfwkj <hswfwkj@163.com>
2022-10-19 08:16:32 +0800

    树形dp

---
+++ P1122 最大子树和.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 1e6 + 5;
+int n, tot;
+int f[N], a[N];
+int ver[N], nxt[N], head[N];
+inline void add(int x, int y)
+{
+	ver[++tot] = y;
+	nxt[tot] = head[x];
+	head[x] = tot;
+}
+inline void dfs(int x, int fa)
+{
+	f[x] = a[x];
+	for(int i = head[x]; i; i = nxt[i])
+	{
+		int y = ver[i];
+		if(y == fa) continue;
+		dfs(y, x);
+		f[x] += max(f[y], 0);
+	}
+}
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	cin >> n;
+	for(int i = 1; i <= n; i++) cin >> a[i];
+	for(int i = 1; i < n; i++)
+	{
+		int x, y;
+		cin >> x >> y;
+		add(x, y);
+		add(y, x);
+	}
+	dfs(1, 0);
+	int ans = 0;
+	for(int i = 1; i <= n; i++)
+		ans = max(ans, f[i]);
+	cout << ans;
+	return 0;
+}
