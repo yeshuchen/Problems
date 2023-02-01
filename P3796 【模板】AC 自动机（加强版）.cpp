c8d4e0bfba1fe92903f648e5607e6c8e270ebccd
hswfwkj <hswfwkj@163.com>
2023-02-01 08:46:15 +0800

    AC自动机模板

--- P3796 【模板】AC 自动机（加强版）.cpp
+++ P3796 【模板】AC 自动机（加强版）.cpp
@@ -1,62 +1,75 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int N = 5e4 + 5;
-int n, cnt;
-string S[N];
-int ch[N][26];
-int f[N], c[N], ed[N];
-inline void insert(string s, int id)
+int n, res;
+int tr[20005][26];
+int e[20005], fail[20005];
+inline void insert(string s, int x)
 {
-    int p = 0;
-    for(int i = 0; i < s.size(); i++)
-    {
-        if(!ch[p][s[i] - 'a']) ch[p][s[i] - 'a'] = ++cnt;
-        p = ch[p][s[i] - 'a'];
-    }
-    ed[p] = id;
+	int head = 0;
+	for(int i = 0; i < s.size(); i++)
+	{
+		if(!tr[head][s[i] - 'a'])
+			tr[head][s[i] - 'a'] = ++res;
+		head = tr[head][s[i] - 'a'];
+	}
+	e[head] = x;
 }
 inline void build()
 {
-    queue<int> q;
-    for(int i = 0; i < 26; i++)
-        if(ch[0][i])
-            q.push(ch[0][i]);
-    while(!q.empty())
-    {
-        int x = q.front(); q.pop();
-        for(int i = 0; i < 26; i++)
-        {
-            if(ch[x][i]) f[ch[x][i]] = ch[f[x]][i], q.push(ch[x][i]);
-            else ch[x][i] = ch[f[x]][i];
-        }
-    }
+	queue<int> q;
+	for(int i = 0; i < 26; i++)
+		if(tr[0][i])
+			q.push(tr[0][i]);
+	while(!q.empty())
+	{
+		int k = q.front(); q.pop();
+		for(int i = 0; i < 26; i++)
+		{
+			if(tr[k][i])
+			{
+				fail[tr[k][i]] = tr[fail[k]][i];
+				q.push(tr[k][i]);
+			}
+			else tr[k][i] = tr[fail[k]][i];
+		}
+	}
 }
+int T, ans, h[1005];
+string s[1005];
 int main()
 {
-    while(cin >> n)
-    {
-        string s;
-        int ans = 0;
-        if(!n) break;
-        cnt = 0;
-        memset(c, 0, sizeof(c));
-        memset(f, 0, sizeof(f));
-        memset(ch, 0, sizeof(ch));
-        memset(ed, 0, sizeof(ed));
-        for(int i = 1; i <= n; i++)
-            cin >> S[i], insert(S[i], i);
-        cin >> s; build();
-        int p = 0;
-        for(int i = 0; i < s.size(); i++)
-        {
-            int tmp = p = ch[p][s[i] - 'a'];
-            while(tmp) c[ed[tmp]]++, tmp = f[tmp];
-        }
-        for(int i = 1; i <= n; i++) ans = max(ans, c[i]);
-        cout << ans << '\n';
-        for(int i = 1; i <= n; i++)
-            if(c[i] == ans)
-                cout << S[i] << '\n';
-    }
-    return 0;
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	while(cin >> n)
+	{
+		if(!n) break;
+		res = 0;
+		memset(tr, 0, sizeof(tr));
+		memset(h, 0, sizeof(h));
+		memset(e, 0, sizeof(e));
+		memset(fail, 0, sizeof(fail));
+		for(int i = 1; i <= n; i++)
+		{
+			cin >> s[i];
+			insert(s[i], i);
+		}
+		build();
+		string t;
+		cin	>> t;
+		int p = 0;
+		for(int i = 0; i < t.size(); i++)
+		{
+			char x = t[i];
+			p = tr[p][x - 'a'];
+			for(int j = p; j; j = fail[j]) h[e[j]]++;
+		}
+		int maxn = 0;
+		for(int i = 1; i <= n; i++)
+			maxn = max(maxn, h[i]);
+		cout << maxn << '\n';
+		for(int i = 1; i <= n; i++)
+			if(h[i] == maxn)
+				cout << s[i] << '\n';
+	}
+	return 0;
 }
--- P3808 【模板】AC 自动机（简单版）.cpp
+++ P3808 【模板】AC 自动机（简单版）.cpp
@@ -1,54 +1,61 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int N = 1e6 + 5;
-int n, cnt;
-int ed[N], fa[N];
-int ch[N][26];
+int n, res;
+int tr[1000005][27];
+int e[1000005], fail[1000005];
 inline void insert(string s)
 {
-    int node = 0;
-    for(int i = 0; i < s.size(); i++)
-    {
-        if(!ch[node][s[i] - 'a']) ch[node][s[i] - 'a'] = ++cnt;
-        node = ch[node][s[i] - 'a'];
-    }
-    ed[node]++;
+	int head = 0;
+	for(int i = 0; i < s.size(); i++)
+	{
+		if(!tr[head][s[i] - 'a'])
+			tr[head][s[i] - 'a'] = ++res;
+		head = tr[head][s[i] - 'a'];
+	}
+	e[head]++;
 }
 inline void build()
 {
-    queue<int> q;
-    for(int i = 0; i < 26; i++)
-        if(ch[0][i])
-            q.push(ch[0][i]);
-    while(!q.empty())
-    {
-        int t = q.front(); q.pop();
-        for(int i = 0; i < 26; i++)
-        {
-            if(ch[t][i]) fa[ch[t][i]] = ch[fa[t]][i], q.push(ch[t][i]);
-            else ch[t][i] = ch[fa[t]][i];
-        }
-    }
+	queue<int> q;
+	for(int i = 0; i < 26; i++)
+		if(tr[0][i])
+			q.push(tr[0][i]);
+	while(!q.empty())
+	{
+		int k = q.front(); q.pop();
+		for(int i = 0; i < 26; i++)
+		{
+			if(tr[k][i])
+			{
+				fail[tr[k][i]] = tr[fail[k]][i];
+				q.push(tr[k][i]);
+			}
+			else tr[k][i] = tr[fail[k]][i];
+		}
+	}
 }
-int p, ans;
-string s;
+int ans;
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
-    cin >> n;
-    for(int i = 1; i <= n; i++)
-    {
-        string s;
-        cin >> s;
-        insert(s);
-    } 
-    cin >> s; build();
-    for(int i = 0 ; i < s.size(); i++)
-    {
-        int tmp = p = ch[p][s[i] - 'a'];
-        while(ed[tmp] != -1) ans += ed[tmp], ed[tmp] = -1, tmp = fa[tmp];
-    }
-    cout << ans;
-    return 0;
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	cin >> n;
+	for(int i = 1; i <= n; i++)
+	{
+		string s;
+		cin >> s;
+		insert(s);
+	}
+	build();
+	string t;
+	cin >> t;
+	int p = 0;
+	for(int i = 0; i < t.size(); i++)
+	{
+		char x = t[i];
+		p = tr[p][x - 'a'];
+		for(int j = p; j && e[j] != -1; j = fail[j]) ans += e[j], e[j] = -1;
+	}
+	cout << ans;
+	return 0;
 }
