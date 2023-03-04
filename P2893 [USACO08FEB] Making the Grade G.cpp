912bfed738cd2d06b81d1c02a3112f12d8bce9f9
yeshuchen <hswfwkj@163.com>
2023-03-04 18:49:43 +0800

    线性dp,用f[i][j]表示第i个位置调整到第j低的高度时的最小代价

--- P2893 [USACO08FEB] Making the Grade G.cpp
+++ P2893 [USACO08FEB] Making the Grade G.cpp
@@ -1,7 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 int n;
-int h[2005], a[2005], f[2005][2005];
+long long ans = LLONG_MAX;
+long long h[2005], a[2005], f[2005][2005];
+inline void solve()
+{
+	for(int i = 1; i <= n; i++)
+		f[1][i] = abs(h[1] - a[i]);
+	for(int i = 2; i <= n; i++)
+	{
+		long long minn = LLONG_MAX;
+		for(int j = 1; j <= n; j++)
+		{
+			minn = min(minn, f[i - 1][j]);
+			f[i][j] = minn + abs(h[i] - a[j]);
+		}
+	}
+	for(int i = 1; i <= n; i++)
+		ans = min(ans, f[n][i]);
+}
 int main()
 {
 	ios::sync_with_stdio(false);
@@ -11,17 +28,9 @@ int main()
 		cin >> h[i];
 	memcpy(a, h, sizeof(h));
 	sort(a + 1, a + n + 1);
-	for(int i = 1; i <= n; i++)
-		f[1][i] = abs(h[1] - a[i]);
-	for(int i = 2, minn = INT_MAX; i <= n; i++, minn = INT_MAX)
-		for(int j = 1; j <= n; j++)
-		{
-			minn = min(minn, f[i - 1][j]);
-			f[i][j] = minn + abs(h[i] - a[j]);
-		}
-	int ans = INT_MAX;
-	for(int i = 1; i <= n; i++)
-		ans = min(ans, f[n][i]);
+	solve();
+	reverse(h + 1, h + n + 1);
+	solve();
 	cout << ans;
 	return 0;
 }
