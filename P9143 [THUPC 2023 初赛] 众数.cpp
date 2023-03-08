e4e1b75ea2095f5f691e49475298ebef156f137b
hswfwkj <hswfwkj@163.com>
2023-03-08 15:00:03 +0800

    更改排序顺序后进行的优化

--- P9143 [THUPC 2023 初赛] 众数.cpp
+++ P9143 [THUPC 2023 初赛] 众数.cpp
@@ -5,9 +5,9 @@ int n;
 struct node{
 	int v, pos;
 }a[100005];
-int ans, sum;
+int ans;
 int f[100005], temp[100005];
-inline bool cmp(node x, node y) {return x.v > y.v;}
+inline bool cmp(node x, node y) {return x.v < y.v;}
 signed main()
 {
 	ios::sync_with_stdio(false);
@@ -17,16 +17,15 @@ signed main()
 		cin >> a[i].v, a[i].pos = i;
 	sort(a + 1, a + n + 1, cmp);
 	for(int i = 1; i <= n; i++)
-		temp[a[i].pos] = i;
-	for(int i = n; i; i--)
-		f[i] = f[i + 1] + a[i].v;
-	int minn = n;
+		temp[a[i].pos] = i,
+		f[i] = f[i - 1] + a[i].v;
+	int maxn = 0;
 	for(int i = n; i; i--)
 	{
 		int p = temp[i];
-		if(p > minn) continue;
-		ans += (f[p] - f[minn + 1] + a[p].v * (p - 1) - sum * minn) * i;
-		minn = p - 1; sum = a[p].v;
+		if(p < maxn) continue;
+		ans += (f[p] - f[maxn - 1] + a[p].v * (n - p) - a[maxn - 1].v * (n - maxn + 1)) * a[p].pos;
+		maxn = p + 1;
 	}
 	cout << ans;
 	return 0;
