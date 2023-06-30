1c99201fc20676f0ac48c7f68b00b4b2e1950c41
yeshuchen <hswfwkj@163.com>
2023-06-30 20:50:00 +0800

    扩展欧拉定理

--- P4139 上帝与集合的正确用法.cpp
+++ P4139 上帝与集合的正确用法.cpp
@@ -1,42 +1,45 @@
 #include<bits/stdc++.h>
-#define int long long
 using namespace std;
-int n = 10000000;
-int phi[10000005];
+inline int Phi(int x)
+{
+	int n = x;
+	for(int i = 2; i * i <= x; i++)
+	{
+		if(x % i != 0) continue;
+		n = n / i * (i - 1);
+		while(x % i == 0) x /= i;
+	}
+    if(x > 1) n = n / x * (x - 1);
+	return n;
+}
 inline int power(int a, int b, int mod)
 {
-    int base = 1;
-    while(b)
-    {
-        if(b & 1) base = base * a % mod;
-        b >>= 1;
-        a = a * a % mod;
-    }
-    return base;
+	int base = 1;
+	while(b)
+	{
+		if(b & 1) base = 1ll * base * a % mod;
+		a = 1ll * a * a % mod;
+		b >>= 1;
+	}
+	return base;
 }
-inline int f(int k)
+inline int solve(int x)
 {
-    if(k <= 2) return 0;
-    int v = phi[k];
-    return power(2, f(v) + v, k);
+	if(x == 1) return 0;
+	int phi = Phi(x);
+	return power(2, solve(phi) + phi, phi);
 }
-signed main()
+int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
-    int T;
-    cin >> T;
-    for(int i = 1; i <= n; i++) phi[i] = i;
-    for(int i = 2; i <= n; i++)
-    {
-        if(phi[i] != i) continue;
-        for(int j = i; j <= n; j += i)
-            phi[j] = phi[j] / i * (i - 1);
-    }
-    while(T--)
-    {
-        int p;
-        cin >> p;
-        cout << f(p) << '\n';
-    }
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	int T;
+	cin >> T;
+	while(T--)
+	{
+		int p;
+		cin >> p;
+		cout << solve(p) << '\n';
+	}
+	return 0;
 }
