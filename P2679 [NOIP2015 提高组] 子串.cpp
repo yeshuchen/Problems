cc75cda09534edbfb9a1a68ae781c2c77067bd79
yeshuchen <hswfwkj@163.com>
2023-09-19 23:16:00 +0800

    正解，前缀和优化dp

--- P2679 [NOIP2015 提高组] 子串.cpp
+++ P2679 [NOIP2015 提高组] 子串.cpp
@@ -5,39 +5,41 @@ const int mod = 1e9 + 7;
 int n, m, k;
 char s[1005], t[1005];
 int f[1005][205][2];
+int sum[1005][205], mat[1005][205];
 signed main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(0); cout.tie(0);
 	cin >> n >> m >> k;
 	for(int i = 0; i <= n; i++)
-		f[i][0][0] = 1;
+		f[i][0][0] = 1, sum[i][0] = 1;
+    for(int i = 1; i <= n; i++)
+        for(int j = 1; j <= min(i, m); j++)
+            sum[i][j] += sum[i - 1][j - 1], sum[i][j] %= mod;
 	for(int i = 1; i <= n; i++) cin >> s[i];
 	for(int i = 1; i <= m; i++) cin >> t[i];
+    s[0] = '@';
+    for(int i = 1; i <= n; i++)
+        for(int j = 1; j <= min(i, m); j++)
+            while(s[i - mat[i][j]] == t[j - mat[i][j]])
+                mat[i][j]++;
 	for(int l = 1; l <= k; l++)
 	{
 		for(int i = 1; i <= n; i++)
-			for(int j = 1; j <= m; j++)
+			for(int j = 1; j <= min(i, m); j++)
 			{
-				if(i < j) continue;
 				f[i][j][1] = f[i - 1][j][1];
-				for(int p = j; p; p--)
-				{
-					if(s[i - j + p] != t[p]) break;
-					f[i][j][1] += f[i - j + p - 1][p - 1][0];
-					f[i][j][1] %= mod;
-				}
+                if(min(i, j) <= mat[i][j]) f[i][j][1] += sum[i - 1][j - 1];
+                else f[i][j][1] += sum[i - 1][j - 1] - sum[i - mat[i][j] - 1][j - mat[i][j] - 1] + mod;
+                f[i][j][1] %= mod;
 			}
 		for(int i = 0; i <= n; i++)
 			for(int j = 0; j <= m; j++)
-				f[i][j][0] = f[i][j][1];
+				f[i][j][0] = f[i][j][1], sum[i][j] = f[i][j][1];
+        for(int i = 1; i <= n; i++)
+            for(int j = 1; j <= m; j++)
+                sum[i][j] += sum[i - 1][j - 1], sum[i][j] %= mod;
 	}
 	cout << f[n][m][0] % mod;
 	return 0;
 }
-/*
-显然，有方程
-s[i] == t[j] s[i - 1][j] + 1; 能与一整个子串匹配时
-s[i] != t[j] s[i - 1][j];
-考虑从此向多个子串递推，如果我能匹配到前面这么多位
-*/
